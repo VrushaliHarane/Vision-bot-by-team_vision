9efb103495eb34dfc69d270356f1c516a06d3410
VrushaliHarane <42660951+VrushaliHarane@users.noreply.github.com>
2020-03-03 00:44:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload=initAll;
+
+var sendbutton;
+function initAll(){
+    sendbutton = document.getElementById("send");
+    sendbutton.onclick = sendmsg;
+}
+
+function sendmsg(){
+    alert();
+}
